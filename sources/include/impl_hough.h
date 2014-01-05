5114aa5bdfb93c55a6ac991e821c4ac8c65e3fce
Alexandru Pana <alex.pana@vertexarmy.org>
2014-01-05 11:36:13 +0000

    Finished Hough Transform

--- sources/include/impl_hough.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "faps.h"
-
-IplImage* process_hough(const cv::Mat& src, int window_size, int peak_threshold = 180);
--- sources/include/impl_ransac_circle.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "faps.h"
-
-IplImage* process_ransac_circle(const cv::Mat& src);
